679564a6e80a69ab3f4054a271c4287a78457c52
Pranshu <swarooppranshu@gmail.com>
2024-04-25 13:05:01 +0530

    Completed Team Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER':'postgres',
+        'PASSWORD':'Password@123',
+        'HOST':'localhost',
     }
 }
 
@@ -126,3 +129,10 @@ STATICFILES_DIRS=[
 
     os.path.join(BASE_DIR,'carzone/static'),
 ]
+
+# Media settings
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+MEDIA_URL='/media/'
+
